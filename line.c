0230928978dd204c6bd7a7e372e022dd22a0e947
KOUSINISHA <35996286+KOUSINISHA@users.noreply.github.com>
2018-03-28 15:54:42 +0530

    line.c

---
+++ line.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char ch;
+printf("Enter the number");
+scanf("%d,&a");
+printf("count the number of line");
+scanf("%d,&b");
+{
+getch();
+}
+};
