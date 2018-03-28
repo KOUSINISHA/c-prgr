5c4d81c0cad8fd090a1138687c5b21833fe09509
KOUSINISHA <35996286+KOUSINISHA@users.noreply.github.com>
2018-03-28 16:43:42 +0530

    swap.c

---
+++ swap.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int=a,b;
+printf("Enter the number");
+scanf("%d,&a");
+printf("Enter the 2 swap numbers"):
+scanf("%d,&b");
+{
+return(0)
+}
+};
