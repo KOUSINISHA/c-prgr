7bbd9760ebf90e3246a67d593398c7e48fa400e3
KOUSINISHA <35996286+KOUSINISHA@users.noreply.github.com>
2018-03-28 16:24:22 +0530

    special.c

---
+++ special.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#incluide<conio.h>
+int main()
+{
+int=a,b;
+printf("Enter the number"):
+scanf("%d,&a");
+printf("number of special character");
+scanf("%d,&b");
+{
+return(0);
+}
+getch();
+}
